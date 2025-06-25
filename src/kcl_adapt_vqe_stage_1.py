3cf13b8f6fa0e8112741166981812d3b4cdcd1d4
Karine EM <karineven79@gmail.com>
2025-06-25 15:28:00 +0100

    Update kcl_adapt_vqe_stage_1.py

--- src/kcl_adapt_vqe_stage_1.py
+++ src/kcl_adapt_vqe_stage_1.py
@@ -1,3 +1,14 @@
+"""
+This script is a wrapper for ADPT-QSCI QE implementation for phase 1 (data augmentation)
+
+This file is part of the AccelerQ Project.
+(2025) King's College London. CC BY 4.0.
+- You must give appropriate credit, provide a link to the license, and indicate if changes 
+  were made. You may do so in any reasonable manner, but not in any way that suggests 
+  the licensor endorses you or your use.
+
+"""
+
 import sys
 import os
 import numpy as np
