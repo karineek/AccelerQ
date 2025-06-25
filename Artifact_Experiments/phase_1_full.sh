d1f1c7bd44f9c56bf91045a50349ee2820ea7674
Karine EM <karineven79@gmail.com>
2025-06-25 09:50:09 +0100

    Create phase_1_full.sh

---
+++ Artifact_Experiments/phase_1_full.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+prefixes=(
+    02qubits_05
+    04qubits_00 04qubits_01 04qubits_02 04qubits_03 04qubits_04 04qubits_05
+    06qubits_05 06qubits_06 06qubits_07
+    07qubits_05 07qubits_06
+    08qubits_05 08qubits_06
+    10qubits_05 10qubits_06
+    12qubits_00 12qubits_01 12qubits_02 12qubits_03 12qubits_04 12qubits_05
+    14qubits_05 14qubits_06
+    16qubits_05 16qubits_06
+)
+
+cd ../src/
+scripts=(
+    kcl_QCELS_stage_1.py
+    kcl_adapt_vqe_stage_1.py
+)
+
+for script in "${scripts[@]}"; do
+  cp "$script" "$script.original"
+done
+
+for prefix in "${prefixes[@]}"; do
+  for script in "${scripts[@]}"; do
+    echo ">> Running $script with prefix $prefix"
+    token=$(grep "Change only this!" "$script" | cut -d'"' -f2)
+    sed -i "s|$token|$prefix|g" "$script"
+    python3 $script
+  done
+done
+
+echo ">> Done"
