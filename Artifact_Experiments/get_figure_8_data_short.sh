c52e36ffdb3bafc209b0908c781aadaa394c88ff
Karine EM <karineven79@gmail.com>
2025-06-27 17:34:46 +0100

    Update get_figure_8_data_short.sh

--- Artifact_Experiments/get_figure_8_data_short.sh
+++ Artifact_Experiments/get_figure_8_data_short.sh
@@ -1,8 +1,31 @@
 cd ../src
 
-## QCELS:
+## ADAPT: 10 Hours
+firstSTR=`cat first_answer_experiments.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
+sed -i "s:$firstSTR:n_qubits=20,seed=5,:g" first_answer_experiments.py
+for i in {1..2}; do python3 first_answer_experiments.py; done
+sed -i "s:is_OPT = True:is_OPT = False:g" first_answer_experiments.py
+for i in {1..2}; do python3 first_answer_experiments.py; done
+sed -i "s:is_OPT = False:is_OPT = True:g" first_answer_experiments.py
+
+firstSTR=`cat first_answer_experiments.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
+sed -i "s:$firstSTR:n_qubits=24,seed=7,:g" first_answer_experiments.py
+for i in {1..2}; do python3 first_answer_experiments.py; done
+sed -i "s:is_OPT = True:is_OPT = False:g" first_answer_experiments.py
+for i in {1..2}; do python3 first_answer_experiments.py; done
+sed -i "s:is_OPT = False:is_OPT = True:g" first_answer_experiments.py
+
+firstSTR=`cat first_answer_experiments-tests.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
+sed -i "s:$firstSTR:n_qubits=20,seed=5,:g" first_answer_experiments-tests.py
+for i in {1..2}; do python3 first_answer_experiments-tests.py; done
+
+firstSTR=`cat first_answer_experiments-tests.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
+sed -i "s:$firstSTR:n_qubits=24,seed=7,:g" first_answer_experiments-tests.py
+for i in {1..2}; do python3 first_answer_experiments-tests.py; done
+
+## QCELS: 36 hours
 firstSTR=`cat QCELS_answer_experiments.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
-sed -i "s:$firstSTR:n_qubits=24,seed=6,:g" QCELS_answer_experiments.py
+sed -i "s:$firstSTR:n_qubits=20,seed=5,:g" QCELS_answer_experiments.py
 for i in {1..2}; do python3 QCELS_answer_experiments.py; done
 sed -i "s:is_OPT = True:is_OPT = False:g" QCELS_answer_experiments.py
 for i in {1..2}; do python3 QCELS_answer_experiments.py; done
@@ -16,30 +39,7 @@ for i in {1..2}; do python3 QCELS_answer_experiments.py; done
 sed -i "s:is_OPT = False:is_OPT = True:g" QCELS_answer_experiments.py
 
 firstSTR=`cat first_answer_experiments-tests.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
-sed -i "s:$firstSTR:n_qubits=24,seed=6,:g" first_answer_experiments-tests.py
-for i in {1..2}; do python3 first_answer_experiments-tests.py; done
-
-firstSTR=`cat first_answer_experiments-tests.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
-sed -i "s:$firstSTR:n_qubits=24,seed=7,:g" first_answer_experiments-tests.py
-for i in {1..2}; do python3 first_answer_experiments-tests.py; done
-
-## ADAPT:
-firstSTR=`cat first_answer_experiments.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
-sed -i "s:$firstSTR:n_qubits=24,seed=6,:g" first_answer_experiments.py
-for i in {1..2}; do python3 first_answer_experiments.py; done
-sed -i "s:is_OPT = True:is_OPT = False:g" first_answer_experiments.py
-for i in {1..2}; do python3 first_answer_experiments.py; done
-sed -i "s:is_OPT = False:is_OPT = True:g" first_answer_experiments.py
-
-firstSTR=`cat first_answer_experiments.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
-sed -i "s:$firstSTR:n_qubits=24,seed=7,:g" first_answer_experiments.py
-for i in {1..10}; do python3 first_answer_experiments.py; done
-sed -i "s:is_OPT = True:is_OPT = False:g" first_answer_experiments.py
-for i in {1..2}; do python3 first_answer_experiments.py; done
-sed -i "s:is_OPT = False:is_OPT = True:g" first_answer_experiments.py
-
-firstSTR=`cat first_answer_experiments-tests.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
-sed -i "s:$firstSTR:n_qubits=24,seed=6,:g" first_answer_experiments-tests.py
+sed -i "s:$firstSTR:n_qubits=20,seed=5,:g" first_answer_experiments-tests.py
 for i in {1..2}; do python3 first_answer_experiments-tests.py; done
 
 firstSTR=`cat first_answer_experiments-tests.py | grep "print(run_algorithm.get_result(n_qubits=" | cut -d'(' -f3 | cut -d' ' -f1`
